001f00cd2757a240438c742db8625438122d6580
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-03-27 23:32:44 +0100

    Feature: added a fuction for to know what is

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,4 +2,15 @@ const returnNumber = (number) => number;
 
 const isMultipleOf = (multiple, number) => number % multiple == 0;
 
-module.exports = { returnNumber, isMultipleOf };
+const whatIs = (number) => {
+  const isMultipleOf3 = isMultipleOf(3, number);
+  const isMultipleOf5 = isMultipleOf(5, number);
+
+  if (isMultipleOf3 && isMultipleOf5) return "FizzBuzz";
+  if (isMultipleOf3) return "Fizz";
+  if (isMultipleOf5) return "Buzz";
+
+  return number;
+};
+
+module.exports = { returnNumber, isMultipleOf, whatIs };
