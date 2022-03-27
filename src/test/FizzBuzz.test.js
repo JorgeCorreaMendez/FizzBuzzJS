aeb6fe77fe24d0f999cd0d85cbe8c2b6268bf4d2
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-03-27 23:40:36 +0100

    Feature: added diferent test for whatIs

--- src/test/FizzBuzz.test.js
+++ src/test/FizzBuzz.test.js
@@ -16,3 +16,23 @@ test("should be multiple of 5", () => {
   if(!fizzBuzz.isMultipleOf(5, 10))
     throw new Error("fuction should be true but got false ")
 })
+
+test("should be fizz", () => {
+  const number = fizzBuzz.whatIs(3) 
+  if(number != "Fizz")
+    throw new Error("fuction should be Fizz but got " + number)
+})
+
+test("should be buzz", () => {
+  const number = fizzBuzz.whatIs(5) 
+  if(number != "Buzz")
+    throw new Error("fuction should be Buzz but got " + number)
+})
+
+test("should be fizzbuzz", () => {
+  const number = fizzBuzz.whatIs(15) 
+  if(number != "FizzBuzz")
+    throw new Error("fuction should be Buzz but got " + number)
+})
+
+
